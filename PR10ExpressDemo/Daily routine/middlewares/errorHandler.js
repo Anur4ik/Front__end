310bbbdc4e307bf0d49de897aec935ec80f54e69
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-06-05 18:12:11 +0300

    Delete PR10ExpressDemo/Daily routine directory

--- PR10ExpressDemo/Daily routine/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const logEvent = require('./logEvents');
-
-const errorHandler = (err, req, res, next) => {
-    logEvent(`${err.name}: ${err.message}`, 'errors.txt');
-    console.error(err.stack)
-    res.status(500).send(err.message);
-}
-
-module.exports = errorHandler;
